3c37cb968b7a78e297f3bfcba627a0174ff4afee
JohnBenedictToquero-UCM <roan94306@gmail.com>
2025-09-18 05:33:23 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        //The Smiths - Please, Please, Please, Let Me Get What I Want
+        System.out.println("So, for once in my life");
+        System.out.println("Let me get what I want");
+        System.out.println("Lord knows it would be the first time");
+        System.out.println("Lord knows it would be the first time");
+    }
+}
